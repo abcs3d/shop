1701617f1df07337a82d3455ad35f6de975e6828
abcs3d <89616784+abcs3d@users.noreply.github.com>
2021-09-06 12:25:03 -0500

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+    <header className="row">
+        <div>
+            <a className="brand" href="index.html">Free shop</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign in</a>
+        </div>
+    </header>
+
+    <main>
+    <Route path="/product/:id" component={ProductScreen} ></Route>
+      <Route path="/" component={HomeScreen} excat></Route>
+        
+    </main>
+    <footer className="row center">
+        All rights reserved
+    </footer>
+
+</div>
+</BrowserRouter>
+  );
+}
+
+export default App;
